d62e80a19cc913896e345ba45bc88681c45ec338
Tomek <01197634@pw.edu.pl>
2026-09-17 18:56:34 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/pl/computer_store_app/ComputerStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.pl.computer_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/computer_store_app/ComputerStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.computer_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
